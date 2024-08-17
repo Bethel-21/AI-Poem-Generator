f2980fd1c0b1093dd383a30bc3efb2304a1549a8
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2024-08-17 10:43:14 +0300

    Added a user experience

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function showPoem(response) {
+
+
+ 
   new Typewriter(".poem-place", {
     strings: response.data.answer,
     autoStart: true,
-    cursor: ""
+    delay: 1,
+    cursor: "",
+    
   });
 }
 
@@ -11,6 +16,10 @@ function callAPI(prompt) {
   let context =
     "I’m in the mood for a bit of creative expression. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure the poem is abaout the word I provided. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+   let poemElement = document.querySelector(".poem-place");
+   poemElement.classList.remove("hidden");
+   poemElement.innerHTML = `<div class="generating">⏳ Generating a poem ⏳</div>`;
   axios.get(apiUrl).then(showPoem);
 }
 
